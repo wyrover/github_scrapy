048a5a6e587524341362492dc78b9c6419f8d6cd
wyrover <wyrover@gmail.com>
2014-08-22 11:57:31 +0800

    + run.bat

--- github_scrapy/spiders/github.py
+++ github_scrapy/spiders/github.py
@@ -14,35 +14,51 @@ import json
 class GithubSpider(Spider):
     name = "github"
     allowed_domains = ["github.com"]
-    start_urls = ["https://github.com/jakubroztocil/httpie/network/members"]
+    start_urls = ["https://github.com/angular/angular.js/network/members"]
+    crawledLinks = []
+    out_links = []
 
     def parse(self, response):
         selector = HtmlXPathSelector(response)
         pageurls = selector.xpath('//div[@class="repo"]/a[1]/@href').extract()
         for url in pageurls:           
             url = 'https://github.com' + url + '?tab=repositories'
-            yield Request(url, callback = self.parse_user)
+            if not url in self.crawledLinks:
+                self.crawledLinks.append(url)
+                yield Request(url, callback = self.parse_user)
 
 
 
     def parse_user(self, response):        
-        regex_selector = re.compile('language">(.*?)</li>[^\b]+?class="repolist-name">[^\b]+?<a\s+href="(.*?)">(.*?)</a>', re.MULTILINE)
+        regex_selector = re.compile('language">(.*?)</li>[^\b]+?class="repolist-name">[^\b]+?<a\s+href="(.*?)".*?>(.*?)</a>', re.MULTILINE)
         matches = regex_selector.findall(response.body)
 
         if matches:
-            for item in matches:                       
-                text = "<tr><td>%s</td><td><a href=\"https://github.com%s\">%s</a></td></tr>" % (item[0], item[1], item[2])
-                self.f_writer_.write('%s\n' % text)   
-                self.csv_writer_.write('%s,https://github.com%s,%s\n' % (item[0], item[1], item[2]))
+            for item in matches:     
+                sublink = item[1]
+                if not sublink in self.out_links:
+                    self.out_links.append(sublink)
+                    text2 = "%s,https://github.com%s,%s" % (item[0], item[1], item[2])
+                    text = "<tr><td>%s</td><td><a href=\"https://github.com%s\">%s</a></td></tr>" % (item[0], item[1], item[2])
+                    self.csv_writer_.write('%s\n' % text2)
+                    self.f_writer_.write('%s\n' % text)   
+                    
 
-        #name_list = selector.xpath('//h3[@class="repolist-name"]/a/text()').extract()
-        #url_list = selector.xpath('//h3[@class="repolist-name"]/a/@href').extract()        
-        #print name, suburl
+        
 
     def __init__(self):
-        self.f_writer_ = open('github.html','w')
+        html_filename = "test.html"
+        csv_filename = "test.csv"
+
+        result = re.search(r'https:\/\/.*?\/.*?\/(.*?)\/', self.start_urls[0], re.IGNORECASE) 
+        if result:
+            #print "-----------------------%s" % result.group(1)
+            html_filename = "%s.html" % result.group(1)
+            csv_filename = "%s.csv" % result.group(1)
+
+        self.f_writer_ = open(html_filename,'w')
         self.f_writer_.write('<html><body><table>')
-        self.csv_writer_ = open('github.csv', 'w')
+        self.csv_writer_ = open(csv_filename, 'w')
 
     def __del__(self):
         self.csv_writer_.close()
