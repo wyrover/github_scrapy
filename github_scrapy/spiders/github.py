138ff5acb8b718f61ba3271514e7c07e907649e6
wyrover <wyrover@gmail.com>
2015-02-09 18:51:05 +0800

    * run.bat

--- github_scrapy/spiders/github.py
+++ github_scrapy/spiders/github.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import os, sys
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('gb18030') 
 from scrapy.spider import Spider
 from scrapy.utils.url import urljoin_rfc
 from scrapy.selector import HtmlXPathSelector
@@ -14,7 +17,7 @@ import json
 class GithubSpider(Spider):
     name = "github"
     allowed_domains = ["github.com"]
-    start_urls = ["https://github.com/angular/angular.js/network/members"]
+    start_urls = ["https://github.com/JakeWharton/ActionBarSherlock/network/members"]
     crawledLinks = []
     out_links = []
 
@@ -22,29 +25,43 @@ class GithubSpider(Spider):
         selector = HtmlXPathSelector(response)
         pageurls = selector.xpath('//div[@class="repo"]/a[1]/@href').extract()
         for url in pageurls:           
-            url = 'https://github.com' + url + '?tab=repositories'
+            url = 'https://github.com' + url + '/repositories'
             if not url in self.crawledLinks:
                 self.crawledLinks.append(url)
+                self.log_.write('%s\n' % url)
                 yield Request(url, callback = self.parse_user)
 
 
 
     def parse_user(self, response):        
-        regex_selector = re.compile('language">(.*?)</li>[^\b]+?class="repolist-name">[^\b]+?<a\s+href="(.*?)".*?>(.*?)</a>', re.MULTILINE)
+        regex_selector = re.compile('repo-list-stats">([^\b]+?)<a[^\b]+?git-branch"[^\b]+?</span>([^\b]+?)</a>[^\b]+?repo-list-name">[^\b]+?<a\shref="(.*?)">([^\b]+?)</a>', re.MULTILINE)
         matches = regex_selector.findall(response.body)
 
         if matches:
             for item in matches:     
-                sublink = item[1]
+                sublink = item[2]
                 if not sublink in self.out_links:
                     self.out_links.append(sublink)
-                    text2 = "%s,https://github.com%s,%s" % (item[0], item[1], item[2])
-                    text = "<tr><td>%s</td><td><a href=\"https://github.com%s\">%s</a></td></tr>" % (item[0], item[1], item[2])
+                    text2 = "%s,https://github.com%s,%s,%s" % (item[0].strip(), item[2].strip(), item[3].strip(), item[1].strip().replace(',', ''))
+                    text = "<tr><td>%s</td><td><a href=\"https://github.com%s\">%s</a></td><td>%s</td></tr>" % (item[0].strip(), item[2].strip(), item[3].strip(), item[1].strip())
                     self.csv_writer_.write('%s\n' % text2)
                     self.f_writer_.write('%s\n' % text)   
-                    
+
+        page_selector = HtmlXPathSelector(response)
+        url_list = page_selector.xpath('//div[@class="pagination"]/a/@href').extract()
+        for url in url_list:
+            url = 'https://github.com' + url
+            if not url in self.crawledLinks:
+                self.crawledLinks.append(url)
+                self.log_.write('%s\n' % url)
+                yield Request(url, callback = self.parse_user)
+                #print url
+                #os.system("pause")    
 
         
+        #name_list = selector.xpath('//h3[@class="repolist-name"]/a/text()').extract()
+        #url_list = selector.xpath('//h3[@class="repolist-name"]/a/@href').extract()        
+        #print name, suburl
 
     def __init__(self):
         html_filename = "test.html"
@@ -59,8 +76,10 @@ class GithubSpider(Spider):
         self.f_writer_ = open(html_filename,'w')
         self.f_writer_.write('<html><body><table>')
         self.csv_writer_ = open(csv_filename, 'w')
+        self.log_ = open("log.txt", 'w')
 
     def __del__(self):
+        self.log_.close()
         self.csv_writer_.close()
 
         self.f_writer_.write('</table></body></html>')
--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
-mode con cp select=936 >nul
 if [%1] == [] goto runbat_args
 
 :runbat
@@ -7,6 +6,7 @@ color 0A
 @set path=C:\Python27;C:\Python27\Scripts;C:\OpenSSL-Win32\bin;D:\MinGW\bin;D:\tools\editplus\tools\depot_tools;%PATH%
 python -c "import sys;print sys.path"
 cd /d "%~dp0"
+echo.
 echo "开始执行scrapy..."
 scrapy crawl github
 echo "scrapy运行完毕."
